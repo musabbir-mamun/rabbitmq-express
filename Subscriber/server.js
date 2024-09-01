7efd60a6120d5263216475dc26ffbcfd9dfc539a
Musabbir <musabbir.mamun@gmail.com>
2024-09-01 12:24:55 +0600

    Acknowledgment sent to Rabbit MQ from Subscriber/server.js

--- Subscriber/server.js
+++ Subscriber/server.js
@@ -6,33 +6,37 @@ const port = 3001;
 
 receiveMessage();
 
-function receiveMessage(){
-
-    amqp.connect(`amqp://localhost`,(err,connection) => {
-        if(err){
+function receiveMessage() {
+    amqp.connect('amqp://localhost', (err, connection) => {
+        if (err) {
             throw err;
         }
-        connection.createChannel((err, channel)=>{
-            if(err){
+        connection.createChannel((err, channel) => {
+            if (err) {
                 throw err;
             }
-            
-            let queueName = "recharge";
-    
-            channel.assertQueue(queueName,{
-                durable:false
-            });
-    
-            channel.consume(queueName, (msg) => {
-                console.log('Received')
-                console.log(JSON.parse(msg.content));   // Receive JSON
-                //console.log(`Received: ${msg.content.toString()}`);  // Receive plain message
-                
-            })
-        })
-    })
 
+            let queueName = "recharge";
+
+            channel.assertQueue(queueName, {
+                durable: false
+            });
+
+            channel.consume(queueName, (msg) => {
+                if (msg !== null) {
+                    console.log('Received');
+                    console.log(JSON.parse(msg.content));  // Receive JSON
+                    //console.log(`Received: ${msg.content.toString()}`);  // Receive plain message
+                    // Acknowledge the message
+                    channel.ack(msg);
+                }
+            }, {
+                noAck: false  // Ensure acknowledgments are enabled
+            });
+        });
+    });
 }
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
